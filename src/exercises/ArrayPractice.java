00fea0182f24a6ccc65067ba5205c4d7114dabf2
Michael Janks <mjanks@emich.edu>
2022-01-06 11:06:00 -0500

    added ArrayPractice class

---
+++ src/exercises/ArrayPractice.java
@@ -0,0 +1,31 @@
+package exercises;
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        int[] myArr = {1, 1, 2, 3, 5, 8};
+        String myStr = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+
+        System.out.print("All values: ");
+        for(int i=0; i < myArr.length; i++) {
+            System.out.print(myArr[i] + " ");
+        }
+
+        System.out.println();
+        System.out.print("Odd values: ");
+        for(int i=0; i < myArr.length; i++) {
+            if(myArr[i] % 2 != 0) {
+                System.out.print(myArr[i] + " ");
+            }
+        }
+
+        System.out.println();
+        String[] myStringArr = myStr.split(" ");
+        System.out.println(Arrays.toString(myStringArr));
+        myStringArr = myStr.split("\\.");
+        System.out.println(Arrays.toString(myStringArr));
+
+    }
+
+}

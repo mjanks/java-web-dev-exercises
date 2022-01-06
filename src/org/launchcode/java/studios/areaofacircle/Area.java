9b46867ff849fa8d06f73c99242b007b644020a5
Michael Janks <mjanks@emich.edu>
2022-01-06 15:36:06 -0500

    added validation

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -6,12 +6,26 @@ public class Area {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double radius;
-        double area;
+        double radius = 0.0;
+        double area = 0.0;
+        boolean flag = false;
 
-        System.out.println("Enter a radius:");
-        radius = scan.nextDouble();
-        area = Circle.getArea(radius);
-        System.out.println("The area of a circle with radius " + radius + " is: " + area);
+        do {
+            System.out.println("Enter a radius:");
+            try {
+                radius = scan.nextDouble();
+            } catch (Exception e) {
+                System.out.println("Invalid datatype!");
+                break;
+            }
+            flag = true;
+        } while(radius <= 0);
+
+        if(flag) {
+            area = Circle.getArea(radius);
+            System.out.println("The area of a circle with radius " + radius + " is: " + area);
+        } else {
+            System.out.println("Program terminated.");
+        }
     }
 }

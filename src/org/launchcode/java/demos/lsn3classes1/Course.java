0b833898856916ebf3c58ea6c6661f77ae528dbf
mjanks <mjanks10@gmail.com>
2022-01-10 16:19:09 -0500

    completed ch. 4 exercises

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -1,6 +1,12 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+
 public class Course {
     // add three fields
     private String courseName;
+    private String instructorName;
+    private ArrayList<Student> students;
+
+
 }
--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -8,5 +8,6 @@ public class SchoolPractice {
         me.setGpa(4.0);
 
 
+
     }
 }
